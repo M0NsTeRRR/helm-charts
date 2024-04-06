99c8f5a9ed7144945d23c7a079773172f196d019
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2024-04-06 22:17:13 +0000

    feat: add values.schema.json
    Signed-off-by: Ludovic Ortega <ludovic.ortega@adminafk.fr>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# install helm schema gen plugin
+helm plugin install https://github.com/KnechtionsCoding/helm-schema-gen.git --version=0.0.10
+
 # install pre-commit hooks
 pre-commit install
