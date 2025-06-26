0d10035f5586656b8fdd15de88c93db3e887aae8
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2025-06-26 18:52:11 +0000

    fix(devcontainer): nixos setup
    Signed-off-by: Ludovic Ortega <ludovic.ortega@adminafk.fr>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,3 +2,7 @@
 
 # install pre-commit hooks
 pre-commit install
+
+# nixos fix
+# see https://github.com/microsoft/vscode-remote-release/issues/11024
+git config --global gpg.program "/usr/bin/gpg"
